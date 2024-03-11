b66ec3df77b4104c907907fbfd6dd357aec9ffb5
Gabriel Pires <102393858+gabrielboostphoenix@users.noreply.github.com>
2024-03-10 22:29:03 -0300

    Update isAuthenticated.ts

--- src/middleware/isAuthenticated.ts
+++ src/middleware/isAuthenticated.ts
@@ -1,7 +1,8 @@
 // Importing Area
-import { Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { JwtPayload, verify } from 'jsonwebtoken';
 
+// This is a feature that verifies if the access token is valid
 const verifyAccessToken = async (token: string): Promise<string | JwtPayload> => {
 
     // Extracting the API secret information from enviorment variable
@@ -13,9 +14,19 @@ const verifyAccessToken = async (token: string): Promise<string | JwtPayload> =>
 
 };
 
-const checkIfTheUserIsAuthenticated = async (req: any, res: Response, next: NextFunction) => {
+// That's a middleware that checks if exists the JWT in the request
+const checkIfTheUserIsAuthenticated = async (req: Request, res: Response, next: NextFunction) => {
 
-    
+    // Getting all of the authorization informations from request header
+    const authHeader = req.headers.authorization;
+
+    // Checking if the authentication method is bearer
+    if (authHeader?.split(' ')[0] === 'Bearer') {
+
+        // Extracting the access json web token
+        const accessToken = authHeader.split(' ')[1];
+
+    }
 
 };
 
